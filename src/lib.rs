00d8d4487bc1e184bd2cb3e5d12b8e86fce0905b
Olga Gaponenko <ogaponenko3@gmail.com>
2020-08-13 14:18:37 +0200

    Updating to newer juniper macros

--- src/lib.rs
+++ src/lib.rs
@@ -2,65 +2,32 @@
 extern crate juniper;
 
 #[derive(GraphQLObject)]
+#[graphql(scalar = juniper::DefaultScalarValue)]
 pub struct PageInfo {
     pub has_previous_page: bool,
-    pub has_next_page: bool
+    pub has_next_page: bool,
+    pub start_cursor: String,
+    pub end_cursor: String,
 }
 
 #[macro_export]
 macro_rules! relay_connection {
-    ($name:ident, $edgename:ident, $type:ty) => {
-        relay_connection!($name, $edgename, $type, ());
+    ($connection:ident, $edge:ident, $type:ty) => {
+        relay_connection!($connection, $edge, $type, ());
     };
-    ($name:ident, $edgename:ident, $type:ty, $context:ty) => {
-        pub struct $edgename {
-            node: $type,
-            cursor: String
+    ($connection:ident, $edge:ident, $type:ty, $context:ty) => {
+        #[derive(juniper::GraphQLObject)]
+        #[graphql(scalar = juniper::DefaultScalarValue, context = $context)]
+        pub struct $edge {
+            pub node: $type,
+            pub cursor: String
         }
 
-        graphql_object!($edgename: $context |&self| {
-            field node() -> &$type {
-                &self.node
-            }
-
-            field cursor() -> &String {
-                &self.cursor
-            }
-        });
-
-        impl $edgename {
-            #[allow(dead_code)]
-            pub fn new(node: $type, cursor: String) -> $edgename {
-                $edgename {
-                    node: node,
-                    cursor: cursor
-                }
-            }
-        }
-
-        pub struct $name {
-            page_info: $crate::PageInfo,
-            edges: Vec<$edgename>
-        }
-
-        graphql_object!($name: $context |&self| {
-            field page_info() -> &$crate::PageInfo {
-                &self.page_info
-            }
-
-            field edges() -> &Vec<$edgename> {
-                &self.edges
-            }
-        });
-
-        impl $name {
-            #[allow(dead_code)]
-            pub fn new(page_info: $crate::PageInfo, edges: Vec<$edgename>) -> $name {
-                $name {
-                    page_info: page_info,
-                    edges: edges
-                }
-            }
+        #[derive(juniper::GraphQLObject)]
+        #[graphql(scalar = juniper::DefaultScalarValue, context = $context)]
+        pub struct $connection {
+            pub edges: Vec<$edge>,
+            pub page_info: $crate::PageInfo,
         }
     }
 }
